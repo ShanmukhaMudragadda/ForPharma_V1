7d614f05bcb584f7197ff52d72a9c1fea9e7668c
ShanmukhaMudragadda <{your_GitHub_email}>
2025-08-29 11:24:25 +0530

    Changed the tsconfig file

--- dist/routes/tourPlanRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import tenantMiddleware from '@/middlewares/tenantMiddleware';
-import { createTourPlanController } from '@/controllers/tourPlan/createTourPlanController';
-import { getTourPlansController } from '@/controllers/tourPlan/getTourPlanController';
-const router = express.Router();
-router.use(tenantMiddleware);
-router.post('/createTourPlan', createTourPlanController);
-router.get('/getTourPlans', getTourPlansController);
-export default router;
